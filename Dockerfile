e20d8066e2a232be25b6f074d7d6d27de237cffb
Pablo, sometimes <mirazopablo@gmail.com>
2025-06-05 17:04:17 -0300

    #7 Dockerfile working, dockerignore & first try deploy and run

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,14 @@
-# Usar imagen oficial de Python
 FROM python:3.11.13-bookworm
 
-# Establecer el directorio de trabajo
-WORKDIR /app
+WORKDIR /
 
-# Copiar archivos necesarios
 COPY requirements.txt .
-
-# Instalar dependencias
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copiar el resto del código
 COPY . .
 
-# Exponer el puerto usado por Uvicorn/FastAPI
+ENV PYTHONPATH=/app
+
 EXPOSE 8000
 
-# Comando para ejecutar FastAPI con Uvicorn
-CMD ["uvicorn", "main:app", "--reload", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Configuraciones básicas
+IMAGE_NAME=mielia_api
+CONTAINER_NAME=api-mielia
+IMAGE_TAG=v1.0.0
+RESOURCE_GROUP=miel-ia
+LOCATION=eastus
+ACR_NAME=apimielia  # sin mayúsculas
+PORT_CONTAINER=8000
+
+# Login a Azure (si no lo hiciste ya)
+az login
+
+# Login al ACR (ya lo hiciste)
+az acr login --name $ACR_NAME
+
+# Construcción y subida de imagen
+docker build -t $IMAGE_NAME:$IMAGE_TAG .
+docker tag $IMAGE_NAME:$IMAGE_TAG $ACR_NAME.azurecr.io/$IMAGE_NAME:$IMAGE_TAG
+docker push $ACR_NAME.azurecr.io/$IMAGE_NAME:$IMAGE_TAG
+
+# Obtener credenciales del ACR
+USERNAME=$(az acr credential show --name $ACR_NAME --query username -o tsv)
+PASSWORD=$(az acr credential show --name $ACR_NAME --query passwords[0].value -o tsv)
+
+# Desplegar en Azure Container Instance
+az container create \
+  --resource-group $RESOURCE_GROUP \
+  --name $CONTAINER_NAME \
+  --image $ACR_NAME.azurecr.io/$IMAGE_NAME:$IMAGE_TAG \
+  --cpu 1 \
+  --memory 1 \
+  --registry-login-server $ACR_NAME.azurecr.io \
+  --registry-username $USERNAME \
+  --registry-password $PASSWORD \
+  --dns-name-label mielia-api-$RANDOM \
+  --ports $PORT_CONTAINER \
+  --location $LOCATION
